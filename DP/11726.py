0f0a76602dae5ceb02e4d0507ab83c3d65605544
yeonx <71878202+yeonx@users.noreply.github.com>
2022-03-23 15:50:01 +0900

    Update 11726.py

--- DP/11726.py
+++ DP/11726.py
@@ -1,11 +1,12 @@
 n=int(input())
 
 dp=[0]*(n+1)
-
-dp[1]=1
-dp[2]=2
-
-for i in range(3,n+1):
-    dp[i]=dp[i-1]+dp[i-2]
+for i in range(1,n+1):
+    if i==1:
+        dp[i]=1
+    elif i==2:
+        dp[i]=2
+    else:
+        dp[i]=dp[i-1]+dp[i-2]
 
 print(dp[n]%10007)
