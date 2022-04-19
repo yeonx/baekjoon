43f4988b0a9a4bc29e84796e4fa331fbe91be6bc
yeonx <71878202+yeonx@users.noreply.github.com>
2022-04-19 14:22:43 +0900

    Create 11727.py

---
+++ DP/11727.py
@@ -0,0 +1,11 @@
+n=int(input())
+arr=[0]*(n+1)
+for i in range(1,n+1):
+    if i==1:
+        arr[i]=1
+    elif i==2:
+        arr[i]=3
+    else:
+        arr[i]=arr[i-1]+arr[i-2]*2
+
+print(arr[n]%10007)
